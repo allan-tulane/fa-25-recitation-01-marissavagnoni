3da5f04b6fd581465397e83780dcb02f5f639a1d
marissavagnoni <marissavagnoni23@gmail.com>
2025-09-03 10:23:30 -0500

    Add files via upload
    Algorithms Lab 1

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import tabulate
 import time
 ###
 
+
 def linear_search(mylist, key):
 	""" done. """
 	for i,v in enumerate(mylist):
@@ -33,7 +34,15 @@ def _binary_search(mylist, key, left, right):
 	  index of key in mylist, or -1 if not present.
 	"""
 	### TODO
-
+	if left > right:
+		return -1 #base case: interval is empty
+	mid = (left + right) // 2 #calculates the middle index of the search interval
+	if mylist[mid] == key:
+		return mid #returns the middle key if that's the one we are looking for
+	elif key < mylist[mid]:
+		return _binary_search(mylist, key, left, mid -1) #if the key is less than the middle element it must be in the left half of the list
+	else:
+		return _binary_search(mylist, key, mid + 1, right) #if the key is greater than the middle element it must be in the right half
 	###
 
 
@@ -58,7 +67,10 @@ def time_search(search_fn, mylist, key):
 	  search function on this input.
 	"""
 	### TODO
-
+	start = time.time() #record start time in seconds
+	search_fn(mylist, key) #run the search function
+	end = time.time() #record end time in seconds
+	return (end - start) * 1000 #convert to milliseconds
 	###
 
 def compare_search(sizes=[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]):
@@ -77,7 +89,17 @@ def compare_search(sizes=[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]):
 	  for each method to run on each value of n
 	"""
 	### TODO
+	results = []
+	for size in sizes:
+		n = int(size)
+		mylist = list(range(n)) #sorted list from 0 to n-1
+		key = -1 #not in the list, worst-case scenario
 
+		linear_time = time_search(linear_search, mylist, key) #gets linear search time
+		binary_time = time_search(binary_search, mylist, key) #gets binary search time
+
+		results.append((n, linear_time, binary_time)) 
+	return results
 	###
 
 def print_results(results):
@@ -86,4 +108,9 @@ def print_results(results):
 							headers=['n', 'linear', 'binary'],
 							floatfmt=".3f",
 							tablefmt="github"))
+	
+print_results(compare_search())
+
+
+
 
